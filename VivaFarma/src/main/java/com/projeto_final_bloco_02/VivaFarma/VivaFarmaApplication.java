f6ca603e19e3e59e4b286bd06d5a5fe3fa114c11
NayRenata <nayara_renata11@hotmail.com>
2024-12-16 10:16:09 -0300

    Atualização da primeira parte do projeto E-Commerce de uma Farmacia

---
+++ VivaFarma/src/main/java/com/projeto_final_bloco_02/VivaFarma/VivaFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_final_bloco_02.VivaFarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaFarmaApplication.class, args);
+	}
+
+}
