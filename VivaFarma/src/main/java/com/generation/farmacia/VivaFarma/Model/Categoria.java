e16cbb2f00d525b5a4ef337e8ef4beaf5db394b7
NayRenata <nayara_renata11@hotmail.com>
2024-12-16 13:48:18 -0300

    Atualização da segunda parte do projeto

--- VivaFarma/src/main/java/com/generation/farmacia/VivaFarma/Model/Categoria.java
+++ VivaFarma/src/main/java/com/generation/farmacia/VivaFarma/Model/Categoria.java
@@ -1,8 +1,58 @@
 package com.generation.farmacia.VivaFarma.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoriafarma")
 public class Categoria {
-	public static void main(String[] args) {
-		
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é Obrigatório para seguir com o cadastro!") 
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é Obrigatório para seguir com o cadastro!") 
+	@Size(min =5, max = 300,  message = "A mensagem deve ter no minimo 5 caractere e no maximo 300 caractere.")
+	private String descricao;
+	
+	
+	private Boolean status;
+	
+	public Long getId() {
+		return id;
 	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+	
 }
 
--- VivaFarma/src/main/java/com/generation/farmacia/VivaFarma/Repository/CategoriaRepository.java
+++ VivaFarma/src/main/java/com/generation/farmacia/VivaFarma/Repository/CategoriaRepository.java
@@ -1,5 +1,13 @@
 package com.generation.farmacia.VivaFarma.Repository;
 
-public class CategoriaRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.farmacia.VivaFarma.Model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository <Categoria, Long>{
+	
+	public List<Categoria> findAllBYNomeContainingIgnoreCase (String nome);
 
 }
